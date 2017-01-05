093d72c99b2e9910732edf5f8642b0cf1563b441
Kevin Filella <kfilella@espol.edu.ec>
2017-01-04 23:24:55 -0500

    Dialogo solo en primera corrida. Agregado mensaje de salida

--- app/src/main/java/ec/edu/espol/ami_project/MostrarMapa.java
+++ app/src/main/java/ec/edu/espol/ami_project/MostrarMapa.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.ami_project;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -9,8 +10,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -32,6 +35,8 @@ public class MostrarMapa extends AppCompatActivity implements OnMapReadyCallback
     private ArrayAdapter<String> mAdapter;
     private ActionBarDrawerToggle mDrawerToggle;
     private String mActivityTitle;
+    private boolean inicio;
+    private SharedPreferences preferences;
     private Toolbar toolbar;
 
     @Override
@@ -48,18 +53,32 @@ public class MostrarMapa extends AppCompatActivity implements OnMapReadyCallback
         setupDrawer();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
-        myAlert.setMessage("Esta aplicación permite ubicar Bloques y Aulas dentro de ESPOL mediante información gráfica y textual. ¡No más clases atrasadas por no encontrar el aula!")
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                })
-                .setTitle("Bienvenido")
-                .create();
-        myAlert.show();
+        if(inicioApp()) {
+            AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+            myAlert.setMessage("Esta aplicación permite ubicar Bloques y Aulas dentro de ESPOL mediante información gráfica y textual. ¡No más clases atrasadas por no encontrar el aula!")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setTitle("Bienvenido")
+                    .create();
+            myAlert.show();
+        }
     }
+
+    private boolean inicioApp() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean inicio = preferences.getBoolean("inicio", false);
+        if (!inicio) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("inicio", true);
+            editor.commit();
+        }
+        return !inicio;
+    }
+
     private void addDrawerItems() {
         String[] osArray = { "Obtener direcciones", "Buscar bloque/aula" };
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
@@ -127,13 +146,23 @@ public class MostrarMapa extends AppCompatActivity implements OnMapReadyCallback
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we
-     * just add a marker near Africa.
-     */
     @Override
     public void onMapReady(GoogleMap map) {
         map.addMarker(new MarkerOptions().position(new LatLng(-2.1481458,-79.9644885)).title("ESPOL"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-2.1481458,-79.9644885) , 18.0f));
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("¿Salir de la aplicación?")
+                .setCancelable(false)
+                .setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
     }
 }
